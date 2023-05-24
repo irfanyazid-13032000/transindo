3915bdd457bc000d6563617867a6b056f579ccd7
endahnasution <79795077+endahnasution@users.noreply.github.com>
2023-05-24 15:01:43 +0700

    pdf fill name

--- app/Http/Controllers/FillPDFController.php
+++ app/Http/Controllers/FillPDFController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use setasign\Fpdi\Fpdi;
 
+
 class FillPDFController extends Controller
 {
    public function process(Request $request){
-    $nama  ='Wahyu Tqgdiluyl Nasution';
-    $outputfile = public_patch().'dcc.pdf';
+    $nama  ='Wahyu Tqgdiluyl Nasution Raja Guk Guk';
+    $outputfile = public_path().'dcc.pdf';
     $this->fillPDF(public_path().'/master/dcc.pdf',$outputfile,$nama);
     return response()->file($outputfile);
 
@@ -17,16 +18,20 @@ class FillPDFController extends Controller
    }
 
    public function fillPDF($file,$outputfile, $nama){
-     $fpdi = new FDPI ;
+     $fpdi = new FPDI ;
      $fpdi->setSourceFile($file);
      $template = $fpdi->importPage(1);
      $size = $fpdi->getTemplateSize($template);
      $fpdi->AddPage($size['orientation'],array($size['width'],$size['height']));
      $fpdi->useTemplate($template);
-     $top =105;
-     $right = 135;
+     $top =98;
+     $right = 80;
      $name  = $nama;
-     $fpdi->SetFont("helvetica","",17);
+     if(str_word_count($name)>3){
+      $right = $right - (str_word_count($name)*5);
+     }
+     $fpdi->SetFont("helvetica","",30);
+     
      $fpdi->SetTextColor(25,26,25);
      $fpdi->Text($right,$top,$name);
      return $fpdi->Output($outputfile,'F');
