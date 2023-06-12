4c8178edd654001aae67a8005d5995505ba1bc29
Rizki Madya <rizkimadya88@gmail.com>
2023-06-12 11:05:48 +0800

    perbaikan route sertifikat

--- resources/views/partials/sidemenu.blade.php
+++ resources/views/partials/sidemenu.blade.php
@@ -99,7 +99,7 @@
                     </a>
                 </li>
                 <li class="menu-item">
-                    <a href="{{ route('sertifikat.show', Auth::user()->email) }}" class="menu-link">
+                    <a href="" class="menu-link">
                         <div>Cetak Sertifikat   </div>
                     </a>
                 </li>
